2805ee4a86572751ec6fae35cad728abce3e6fe3
RikyPIC <69261588+RikyPIC@users.noreply.github.com>
2023-09-08 10:50:41 -0300

    Add files via upload
    Teste de subida de un ejercicio de python.

---
+++ ejercicio7.py
@@ -0,0 +1,33 @@
+A=0
+A=input("Inserte un numero atómico ")
+A=int(A)
+if A==1:
+    print("Hidrógeno")
+   
+elif A==2:
+    print("Helio")
+    
+elif A==8:
+    print("Oxigeno")
+       
+elif A==11:
+    print("Sodio")
+    
+elif A==13:
+    print("Aluminio")
+    
+elif A==17:
+    print("Cloro")
+
+elif A==26:
+    print("Hierro")
+    
+elif A==79:
+    print("Oro")
+    
+elif A==80:
+    print("Mercurio")
+    
+else:
+    print("Elemento no encontrado")
+    
